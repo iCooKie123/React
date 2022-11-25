93962329f85ea2ec5445dd92a5829dce4679def1
iCooKie123 <aconstantin86@yahoo.com>
2022-11-26 01:45:33 +0200

    schimbari footer

--- React/src/components/Footer.jsx
+++ React/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/footer.css";
 import {BsLinkedin,BsGithub} from 'react-icons/bs'
+import {AiOutlineMail} from 'react-icons/ai'
 function Footer(){
     return(
         <footer>
@@ -8,14 +9,17 @@ function Footer(){
                 <ul className="list-right">
                     <li>Contact</li>
                     <div className="contact-flex">
-                    <BsLinkedin/>
-                    <a href="/#">Linkedin</a>
+                        <AiOutlineMail/>
+                        <a href="mailto:aconstantin86@yahoo.com">E-mail</a>
                     </div>
                     <div className="contact-flex">
-                    <BsGithub/>
-                    <a href="https://github.com/iCooKie123">Github</a>
+                        <BsLinkedin/>
+                        <a href="/#">Linkedin</a>
                     </div>
-                    
+                    <div className="contact-flex">
+                        <BsGithub/>
+                        <a href="https://github.com/iCooKie123">Github</a>
+                    </div>
                 </ul>
             </div>
             <div className="ft-left">
